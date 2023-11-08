650c3c8ea79628936c47e4fcea1dcdac2e28e60f
jjalixto <jjalixtoc@gmail.com>
2023-11-08 13:44:24 -0500

    agreando los demas cambios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -14,6 +14,24 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+
+        <div class="mt-4">
+            <x-input-label for="rol" :value="__('¿Que tipo de cuenta deseas en DevJobs?')" />
+
+            <select
+                id="rol"
+                name="rol"
+                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full"
+            >
+                <option value="">-- Selecioname --</option>
+                <option value="1">Developer - Obtener Empleo</option>
+                <option value="2">Recluter - Publicar Empleos</option>
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+            </select>
+
         </div>
 
         <!-- Password -->
@@ -30,7 +48,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Repetir Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -39,14 +57,21 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex justify-between my-8">
+            <x-link
+                :href="route('login')"
+            >
+                Iniciar Sesión
+            </x-link>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <x-link
+                :href="route('password.request')"
+            >
+                Olvidaste tu password
+            </x-link>
         </div>
+        <x-primary-button class="w-full justify-center">
+            {{ __('Crear Cuenta') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
---
+++ resources/views/livewire/crear-vacante.blade.php
@@ -0,0 +1,130 @@
+<form class="md:w-1/2 space-y-5" wire:submit.prevent='crearVacante'>
+
+    <div>
+        <x-input-label for="titulo" :value="__('Título Vacante')" />
+        <x-text-input
+        id="titulo"
+        class="block mt-1 w-full"
+        type="text"
+        wire:model="titulo"
+        :value="old('titulo')"
+        placeholder="Título Vacante" />
+
+        @error('titulo')
+        {{-- sus componentes de livewire son dinamicos --}}
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
+    </div>
+
+    <div>
+        <x-input-label for="salario" :value="__('Salario Mensual')" />
+
+        <select
+                id="salario"
+                wire:model="salario"
+                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full"
+            >
+            <option>-- Seleccione --</option>
+            @foreach ($salarios as $salario)
+                <option value="{{$salario->id}}">{{$salario->salario}}</option>
+            @endforeach
+
+        </select>
+        @error('salario')
+        {{-- sus componentes de livewire son dinamicos --}}
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
+    </div>
+
+    <div>
+        <x-input-label for="categoria" :value="__('Categoría')" />
+
+        <select
+                id="categoria"
+                wire:model="categoria"
+                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full"
+            >
+
+            <option>-- Seleccione --</option>
+            @foreach ($categorias as $categoria)
+                <option value="{{$categoria->id}}">{{$categoria->categoria}}</option>
+            @endforeach
+        </select>
+        @error('categoria')
+        {{-- sus componentes de livewire son dinamicos --}}
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
+    </div>
+
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input
+            id="empresa"
+            class="block mt-1 w-full"
+            type="text"
+            wire:model="empresa"
+            :value="old('empresa')"
+            placeholder="Empresa: ej.Netflix, Uber, Shopify" />
+
+            @error('empresa')
+            {{-- sus componentes de livewire son dinamicos --}}
+                <livewire:mostrar-alerta :message="$message"/>
+            @enderror
+    </div>
+
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último Día para postularse')" />
+        <x-text-input
+        id="ultimo_dia"
+        class="block mt-1 w-full"
+        type="date"
+        wire:model="ultimo_dia"
+        :value="old('ultimo_dia')"
+        placeholder="Empresa: ej.Netflix, Uber, Shopify" />
+
+        @error('ultimo_dia')
+            {{-- sus componentes de livewire son dinamicos --}}
+                <livewire:mostrar-alerta :message="$message"/>
+            @enderror
+    </div>
+
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción Puesto')" />
+        <textarea
+        wire:model="descripcion"
+        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full h-72"
+        placeholder="Descripcion general del puesto, experiencia"></textarea>
+
+        @error('descripcion')
+            {{-- sus componentes de livewire son dinamicos --}}
+                <livewire:mostrar-alerta :message="$message"/>
+        @enderror
+    </div>
+
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+        <x-text-input
+        id="imagen"
+        class="block mt-1 w-full"
+        type="file"
+        wire:model="imagen"
+        accept="image/*" />
+
+        <div class="my-5 w-96">
+            {{-- esta variable va a estar disponible para el front-end  --}}
+            @if ($imagen)
+                Imagen:
+                {{-- temporaryUrl hace que se pueda visualizar la imagen --}}
+                <img src="{{ $imagen->temporaryUrl() }}" >
+            @endif
+        </div>
+
+        @error('imagen')
+        {{-- sus componentes de livewire son dinamicos --}}
+            <livewire:mostrar-alerta :message="$message"/>
+    @enderror
+    </div>
+    <x-primary-button>
+        Crear Cuenta
+    </x-primary-button>
+</form>
